a676214ea2a79e7e2faed41e3b230590df8f9607
bharathiec <bharathiec04@gmail.com>
2023-11-22 18:50:29 +0530

    Hello

---
+++ src/test/java/org/info/Employee.java
@@ -0,0 +1,17 @@
+package org.info;
+
+public class Employee {
+	
+	private void test1() {
+System.out.println("good");
+	}
+
+	private void test2() {
+System.out.println("good");
+	}
+
+	private void test3() {
+System.out.println("good");
+	}
+
+}
